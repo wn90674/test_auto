62a40af17523baa1b63dbc64eb9a739b9aae03fb
wn90674 <maple0421@163.com>
2019-12-29 13:10:55 +0800

    :lock:init

---
+++ 1.ts
@@ -0,0 +1,15 @@
+class Animal {
+     private name: string;
+     _age: number;
+    constructor(theName: string,age: number) {
+        this.name=theName ;
+        this._age=age ;
+    }
+    say(){
+        console.log('123')
+    }
+}
+
+
+let dog=new Animal('gg',4)
+console.log(dog);
